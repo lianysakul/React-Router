6647c61093f50451f0f48a9cd29e10d7eb5953dd
lianysakul <s2200363@student.unklab.ac.id>
2023-10-25 23:34:20 +0800

    first commit

---
+++ components/About.js
@@ -0,0 +1,15 @@
+function About() {
+	return(
+		<div>
+			<h2>Fornt-End web development is the development of the graphical
+			user interface of a website, through the use of HTML, CSS, and JavaScript,
+			so that users can view and intereact with that website.</h2>
+			
+			Read more the article:
+			<a href=
+			"https://frontendmasters.com/guides/front-end-handbook/2019/">
+				https://forntendmasters.com/guides/front-end-handbook/2019/
+			</a>
+		</div>
+	);
+}
